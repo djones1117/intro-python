e9f3cef9e2fb6a8276be0359fef1640d3219c3fc
djones1117 <socjones21@yahoo.com>
2023-10-03 10:25:09 -0400

    setup

---
+++ main.py
@@ -0,0 +1,20 @@
+8 > 8
+# => False — 8 is not greater than 8.
+
+8 >= 8
+# => True — This checks if 8 is greater than or equal to 8, and they are equal.
+
+8 < 8
+# => False — 8 is not less than 8.
+
+7 == 7
+# => True — 7 is equal to 7.
+
+7 == "7"
+# => False — One is a number and the other is a string.
+
+7 != 7
+# => False — This checks if they aren't equal. Because does 7 equal 7, it's `False`.
+
+6 != 7
+# => True — 6 is not equal to 7.
