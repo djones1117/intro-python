7544f44129628406073ebba95d6cc484923f30d0
djones1117 <socjones21@yahoo.com>
2023-10-09 12:02:45 -0400

    prac py

--- main.py
+++ main.py
@@ -65,15 +65,23 @@ not []
 #   print("Clean the walls! They're sticky!")
 # else:
 #   print("The walls are clean!")
+color = ""
 
-color = input('Enter "green", "yellow", "red": ').lower()
-print(f'The user entered {color}')
- 
-if color == "green":
-    print('Go!')
-elif color == "yellow":
-    print('Slow Down!')
-elif color == "red":
-    print('Stop!')
-else:
-   print('bogus')
+while color != 'quit':
+
+    color = input('Enter "green", "yellow", "red": ').lower()
+    print(f'The user entered {color}')
+    
+    if color == "green":
+        print('Go!')
+    elif color == "yellow":
+        print('Slow Down!')
+    elif color == "red":
+        print('Stop!')
+    else:
+        print('bogus')
+
+names = ["Tom", "Deborah", "Murray", "Axel"]
+
+for name in names:
+  print(name)
