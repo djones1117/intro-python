a2d2e4ef8cc640a95bf1dad262fd220de6a96599
djones1117 <socjones21@yahoo.com>
2023-10-20 13:07:04 -0400

    daily prac py classes

--- classes.py
+++ classes.py
@@ -5,18 +5,27 @@ nums = [1, 2, 3]
 print(dir(nums))
 
 class Dog():
+    #class attribute shared between all objects of this class
+    next_id = 1
     #initialize the attributes on the 
     #new object
     def __init__(self, name, age=0):
         self.name = name
         self.age = age
+        self.id = Dog.next_id
+        Dog.next_id += 1
 
     def bark(self):
         print(f'{self.name} say woof!' )
 
     def __str__(self):
-        return f'Dog named {self.name} is {self.age} years old'
+        return f'Dog ({self.id}) named {self.name} is {self.age} years old'
     
+    @classmethod
+    def get_total_dogs(cls):
+        return cls.next_id - 1
+    
+
 
 bo = Dog('Bo', 8)
 nova = Dog('Nova')
@@ -24,7 +33,33 @@ print(bo.bark())
 print(nova.age)
 print(bo)
 print(nova)
+print(Dog.get_total_dogs())
+    #subclass of superclass DOG
+class ShowDog(object):
+    #Add additional parameters AFTER those of
+    #the superclass (Dog)
+    #we need the same properties from above first thing you should do for best practice
+    def __init__(self, name, age=0, total_earnings=0):
+        #always call the superclasses init method first
+        Dog.__init__(self, name, age)
+        #now its okay to specialize
+        self.total_earnings = total_earnings
 
+    def add_prize_money(self, amount):
+        self.total_earnings += amount
+        print(f'{self.name}\'s new total earnings are {self.total_earnings}')
+
+    def __str__(self):
+        return f'Dog ({self.id}) named {self.name} is {self.age} years old'
+    
+    def bark(self):
+        print(f'{self.name} say woof!' )
+        
+chewi = ShowDog('chewi', 15, 3000)
+winky = ShowDog('winky', 3, 1000)
+print(winky)
+print(winky.bark())
+print(winky.add_prize_money(500))
 
 class Vehicle():
     
