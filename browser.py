876876ded1611266e943f17fe3bd707a9e5a8455
djones1117 <socjones21@yahoo.com>
2023-10-23 14:51:50 -0400

    web browse with py

---
+++ browser.py
@@ -0,0 +1,14 @@
+import webbrowser
+
+Url=input("Enter a url : ")
+webbrowser.open(Url)
+
+
+webbrowser.open_new('Url')
+
+##opens the webpage in a new tab
+webbrowser.open_new_tab('Url')
+
+
+# ex url below... run the file - then copy this
+# https://github.com/djones1117
