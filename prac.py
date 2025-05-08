b93d70e8855e17fa32952672c58451f88e0905b9
djones1117 <socjones21@yahoo.com>
2025-05-08 14:26:35 -0400

    daily prac

--- prac.py
+++ prac.py
@@ -119,3 +119,13 @@ while countdown > 0:
     countdown -= 1
 
 print("Blast off!")
+
+
+my_list = [1, 2, 2, 3, 4, 4, 5]
+unique_list = []
+
+for num in my_list:
+    if num not in unique_list:
+        unique_list.append(num)
+
+print("List without duplicates:", unique_list)
