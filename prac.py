1bfdac3ac1bf5fdfc9f7323b2bf2d18cf590e280
djones1117 <socjones21@yahoo.com>
2025-05-13 14:26:32 -0400

    daily prac

--- prac.py
+++ prac.py
@@ -136,3 +136,14 @@ if number % 2 == 0:
     print(number, "is even.")
 else:
     print(number, "is odd.")
+
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
