6484c1bed49dc2f2c4c4e196de64de48109bd6ac
djones1117 <socjones21@yahoo.com>
2025-06-19 07:59:10 -0400

    exponentials

--- prac.py
+++ prac.py
@@ -260,3 +260,10 @@ def greet(name):
     print(f"Hello, {name}!")
 
 greet("Alice")
+
+
+def power(base, exponent=2):
+    return base ** exponent
+
+print(power(3))      # 9
+print(power(2, 3))   # 8
