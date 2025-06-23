6eb0a18b1d7e7dbd9a391c64d029c55857edf3a5
djones1117 <socjones21@yahoo.com>
2025-06-23 13:13:49 -0400

    set operations

--- prac.py
+++ prac.py
@@ -267,3 +267,10 @@ def power(base, exponent=2):
 
 print(power(3))      # 9
 print(power(2, 3))   # 8
+
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+common = set1 & set2
+print("Common elements:", common)
