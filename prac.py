e2447790a37aeada43d79111ded761947484acbd
djones1117 <socjones21@yahoo.com>
2025-07-18 14:15:24 -0400

    triangle stars

--- prac.py
+++ prac.py
@@ -334,3 +334,11 @@ else:
         factorial *= i
     print("Factorial of", number, "is", factorial)
 
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
